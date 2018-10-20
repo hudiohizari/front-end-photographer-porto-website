5defaa1e61d7e3dd9696453520db9a94e3555726
Hudio Hizari <hhizari@gmail.com>
2018-10-20 15:09:15 +0700

    Menyelesaikan blog.html

--- javascript/function.js
+++ javascript/function.js
@@ -9,3 +9,18 @@ function toggleShowTag(tagId) {
     	document.getElementById(tagId).style.display = "none";
 	}
 }
+function changeBlogImage(imgId){
+	var slides = document.getElementsByClassName("slide");
+	var slideList = document.getElementsByClassName("image-list");
+
+	var i
+	for (i = 0; i < slides.length; i++) {
+		slides[i].style.display = "none";  
+	}
+	for (i = 0; i < slideList.length; i++) {
+		slideList[i].getElementsByTagName('div')[0].getElementsByTagName('span')[0].className = "";
+	}
+
+	slides[imgId-1].style.display = "block";
+	slideList[imgId-1].getElementsByTagName('div')[0].getElementsByTagName('span')[0].className = "active";
+}
