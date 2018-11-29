0fb987097762080761638913a277e8e5cbc12785
Hudio Hizari <hhizari@gmail.com>
2018-11-29 19:15:53 +0700

    Revisi halaman blog, slides gambar otomatis

--- javascript/function.js
+++ javascript/function.js
@@ -9,21 +9,6 @@ function toggleShowTag(tagId) {
     	document.getElementById(tagId).style.display = "none";
 	}
 }
-function changeBlogImage(imgId){
-	var slides = document.getElementsByClassName("slide");
-	var slideList = document.getElementsByClassName("image-list");
-
-	var i
-	for (i = 0; i < slides.length; i++) {
-		slides[i].style.display = "none";  
-	}
-	for (i = 0; i < slideList.length; i++) {
-		slideList[i].getElementsByTagName('div')[0].getElementsByTagName('span')[0].className = "";
-	}
-
-	slides[imgId-1].style.display = "block";
-	slideList[imgId-1].getElementsByTagName('div')[0].getElementsByTagName('span')[0].className = "active";
-}
 function startLargeImage(imgUrl){
 	$("#large-img").attr("src", imgUrl);
 }
